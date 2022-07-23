f0fe8b64770cf1d275e8dc699f540663eb1d5856
henchiyb <ducnhanyb@gmail.com>
2022-07-23 13:04:05 +0900

    Remove lock in competitionRankingHandler

--- go/isuports.go
+++ go/isuports.go
@@ -1367,11 +1367,11 @@ func competitionRankingHandler(c echo.Context) error {
 	}
 
 	// player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する
-	fl, err := flockByTenantID(v.tenantID)
-	if err != nil {
-		return fmt.Errorf("error flockByTenantID: %w", err)
-	}
-	defer fl.Close()
+	// fl, err := flockByTenantID(v.tenantID)
+	// if err != nil {
+	// 	return fmt.Errorf("error flockByTenantID: %w", err)
+	// }
+	// defer fl.Close()
 	pss := []PlayerScoreRow{}
 	if err := tenantDB.SelectContext(
 		ctx,
