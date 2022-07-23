515190cd64acb84b395e45eac6126efc2d1a73d4
Hoang Minh Quang <hoang.minh.quang@classi.jp>
2022-07-23 15:38:34 +0900

    conns 40

--- go/isuports.go
+++ go/isuports.go
@@ -178,7 +178,9 @@ func Run() {
 		e.Logger.Fatalf("failed to connect db: %v", err)
 		return
 	}
-	adminDB.SetMaxOpenConns(10)
+	adminDB.SetMaxOpenConns(40)
+	adminDB.SetMaxIdleConns(40)
+	adminDB.SetConnMaxLifetime(1 * time.Minute)
 	defer adminDB.Close()
 
 	port := getEnv("SERVER_APP_PORT", "3000")
