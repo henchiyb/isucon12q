ecdf267a19b1f50f6708ec8fbe916261712bac2a
henchiyb <ducnhanyb@gmail.com>
2022-07-23 01:12:29 +0000

    Initial commit

---
+++ sql/admin/10_schema.sql
@@ -0,0 +1,31 @@
+USE `isuports`;
+
+DROP TABLE IF EXISTS `tenant`;
+DROP TABLE IF EXISTS `id_generator`;
+DROP TABLE IF EXISTS `visit_history`;
+
+CREATE TABLE `tenant` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `display_name` VARCHAR(255) NOT NULL,
+  `created_at` BIGINT NOT NULL,
+  `updated_at` BIGINT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+CREATE TABLE `id_generator` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `stub` CHAR(1) NOT NULL DEFAULT '',
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `stub` (`stub`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+CREATE TABLE `visit_history` (
+  `player_id` VARCHAR(255) NOT NULL,
+  `tenant_id` BIGINT UNSIGNED NOT NULL,
+  `competition_id` VARCHAR(255) NOT NULL,
+  `created_at` BIGINT NOT NULL,
+  `updated_at` BIGINT NOT NULL,
+  INDEX `tenant_id_idx` (`tenant_id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
