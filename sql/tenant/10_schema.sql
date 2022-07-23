4158290bed88e8449050b76bdc882a857cd3d5d8
Hoang Minh Quang <hoang.minh.quang@classi.jp>
2022-07-23 11:51:34 +0900

    add index player score

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -30,3 +30,5 @@ CREATE TABLE player_score (
   created_at BIGINT NOT NULL,
   updated_at BIGINT NOT NULL
 );
+
+CREATE INDEX `tenant_id_competition_id_idx` ON player_score (`tenant_id`, `competition_id`, `player_id`)
