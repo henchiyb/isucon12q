3604004007d7640047a5e518d2a763924ac9085e
Hoang Minh Quang <hoang.minh.quang@classi.jp>
2022-07-23 12:23:43 +0900

    add trigger create latest player score

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS competition;
 DROP TABLE IF EXISTS player;
 DROP TABLE IF EXISTS player_score;
+DROP TABLE IF EXISTS latest_player_score;
+DROP TRIGGER IF EXISTS `tr1`;
 
 CREATE TABLE competition (
   id VARCHAR(255) NOT NULL PRIMARY KEY,
@@ -31,4 +33,21 @@ CREATE TABLE player_score (
   updated_at BIGINT NOT NULL
 );
 
-CREATE INDEX `tenant_id_competition_id_idx` ON player_score (`tenant_id`, `competition_id`, `player_id`, `row_num` DESC)
+CREATE INDEX `tenant_id_competition_id_idx` ON player_score (`tenant_id`, `competition_id`, `player_id`);
+
+CREATE TABLE latest_player_score (
+  id VARCHAR(255) NOT NULL,
+  tenant_id BIGINT NOT NULL,
+  player_id VARCHAR(255) NOT NULL,
+  competition_id VARCHAR(255) NOT NULL,
+  score BIGINT NOT NULL,
+  row_num BIGINT NOT NULL,
+  PRIMARY KEY(tenant_id, competition_id, player_id)
+);
+
+-- CREATE TRIGGER tr1 AFTER INSERT ON player_score FOR EACH ROW REPLACE INTO latest_player_score (tenant_id, competition_id, player_id) VALUES (NEW.id, NEW.score, NEW.row_num);
+
+CREATE TRIGGER IF NOT EXISTS tr1 AFTER INSERT ON latest_player_score
+BEGIN
+  INSERT OR REPLACE INTO latest_player_score(id, score, row_num) VALUES(NEW.id, NEW.score, NEW.row_num);
+END;
