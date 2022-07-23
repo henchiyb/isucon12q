9bc5a106b7ce6f69f51ecbf7d944f58952e15811
Hoang Minh Quang <hoang.minh.quang@classi.jp>
2022-07-23 11:58:55 +0900

    bulk insert from csv

--- go/isuports.go
+++ go/isuports.go
@@ -1112,19 +1112,23 @@ func competitionScoreHandler(c echo.Context) error {
 	); err != nil {
 		return fmt.Errorf("error Delete player_score: tenantID=%d, competitionID=%s, %w", v.tenantID, competitionID, err)
 	}
-	for _, ps := range playerScoreRows {
-		if _, err := tenantDB.NamedExecContext(
-			ctx,
-			"INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)",
-			ps,
-		); err != nil {
-			return fmt.Errorf(
-				"error Insert player_score: id=%s, tenant_id=%d, playerID=%s, competitionID=%s, score=%d, rowNum=%d, createdAt=%d, updatedAt=%d, %w",
-				ps.ID, ps.TenantID, ps.PlayerID, ps.CompetitionID, ps.Score, ps.RowNum, ps.CreatedAt, ps.UpdatedAt, err,
-			)
-
-		}
+	query := "INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)"
+	if _, err := tenantDB.NamedExecContext(ctx, query, playerScoreRows); err != nil {
+		return fmt.Errorf("bulk insert failed, %w", err)
 	}
+	// for _, ps := range playerScoreRows {
+	// 	if _, err := tenantDB.NamedExecContext(
+	// 		ctx,
+	// 		"INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)",
+	// 		ps,
+	// 	); err != nil {
+	// 		return fmt.Errorf(
+	// 			"error Insert player_score: id=%s, tenant_id=%d, playerID=%s, competitionID=%s, score=%d, rowNum=%d, createdAt=%d, updatedAt=%d, %w",
+	// 			ps.ID, ps.TenantID, ps.PlayerID, ps.CompetitionID, ps.Score, ps.RowNum, ps.CreatedAt, ps.UpdatedAt, err,
+	// 		)
+
+	// 	}
+	// }
 
 	return c.JSON(http.StatusOK, SuccessResult{
 		Status: true,
