a99e4d776b41b61b4bfc11d1669e9e5a831feb92
Hoang Minh Quang <hoang.minh.quang@classi.jp>
2022-07-23 13:16:33 +0900

    add limit to competition

--- go/isuports.go
+++ go/isuports.go
@@ -1406,9 +1406,10 @@ func competitionRankingHandler(c echo.Context) error {
 	if err := tenantDB.SelectContext(
 		ctx,
 		&pss,
-		"SELECT * FROM player_score WHERE tenant_id = ? AND competition_id = ? ORDER BY row_num DESC",
+		"SELECT * FROM latest_player_score WHERE tenant_id = ? AND competition_id = ? ORDER BY score DESC, row_num DESC LIMIT ?",
 		tenant.ID,
 		competitionID,
+		100 + int(rankAfter),
 	); err != nil {
 		return fmt.Errorf("error Select player_score: tenantID=%d, competitionID=%s, %w", tenant.ID, competitionID, err)
 	}
